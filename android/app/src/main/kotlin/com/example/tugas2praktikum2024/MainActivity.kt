c013716d5d6034c1b0fe937202750d73f1a7464a
Panji Bagaskara <panji.bagaskara@mhs.unsoed.ac.id>
2024-11-18 21:17:22 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tugas2praktikum2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas2praktikum2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/tampil_data.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class TampilData extends StatelessWidget {
+  final String nama;
+  final String nim;
+  final int tahun;
+
+  const TampilData({
+    Key? key,
+    required this.nama,
+    required this.nim,
+    required this.tahun,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int umur = DateTime.now().year - tahun;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Perkenalan"),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
+        elevation: 2,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Colors.deepPurple, Colors.blueAccent],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Text(
+                "Halo, nama saya $nama,\nNIM $nim,\numur saya $umur tahun.",
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  height: 1.5,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
